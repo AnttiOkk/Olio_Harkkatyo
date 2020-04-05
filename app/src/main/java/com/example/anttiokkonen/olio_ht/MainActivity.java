dd0b663bd80dea0f6b19417a2d39a7a37ea9b948
AnttiOkk <54837742+AnttiOkk@users.noreply.github.com>
2020-04-05 13:20:59 +0300

    Initial commit

--- app/src/main/java/com/example/anttiokkonen/olio_ht/MainActivity.java
+++ app/src/main/java/com/example/anttiokkonen/olio_ht/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.anttiokkonen.olio_ht;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     private TextView mTextMessage;
 
@@ -39,6 +43,32 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        button = (Button) findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityNewPayment();
+            }
+        });
+
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityTransactions();
+            }
+        });
+
+
     }
 
+    public void openActivityNewPayment() {
+        Intent intent = new Intent(this, NewPaymentActivity.class);
+        startActivity(intent);
+    }
+    public void openActivityTransactions() {
+        Intent intent = new Intent(this, TransactionsActivity.class);
+        startActivity(intent);
+    }
 }
